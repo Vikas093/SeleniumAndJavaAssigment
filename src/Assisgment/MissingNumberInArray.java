0474a94f7620073bdaf16b2933e4bf4e917a4aa5
Vikas093 <vikaspaliwal93us@hotmail.com>
2020-04-19 14:49:12 +0530

    Added missing no in array and frame program for selenium

---
+++ src/Assisgment/MissingNumberInArray.java
@@ -0,0 +1,30 @@
+package Assisgment;
+
+public class MissingNumberInArray {
+	
+	
+	//-------------------Program to find missing no in an array------------------------
+	
+	 static int getMissingNo(int a[], int n) 
+	    { 
+	        int i, total; 
+	        total = (n + 1) * (n + 2) / 2; 
+	        for (i = 0; i < n; i++) 
+	            total =total- a[i]; 
+	        return total; 
+	    } 
+
+	
+	  
+	    /* program to test above function */
+	    public static void main(String args[]) 
+	    { 
+	        int a[] = { 1, 2, 3,4, 5, 6 ,7,8,10}; 
+	        int miss = getMissingNo(a, 9); 
+	        System.out.println(miss); 
+	    } 
+	 
+
+	}
+
+
